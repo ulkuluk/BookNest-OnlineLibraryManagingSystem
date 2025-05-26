bc799850cab51cf58496d732dd7555ab4d765c8f
Alfi <alfiram.03@gmail.com>
2025-05-26 17:34:57 +0700

    update

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -145,9 +145,9 @@ async function loginHandler(req, res) {
         );
         res.cookie("refreshToken", refreshToken, {
           httpOnly: true, //ngatur cross-site scripting, untuk penggunaan asli aktifkan karena bisa nyegah serangan fetch data dari website "document.cookies"
-          sameSite: "lax", //ini ngatur domain yg request misal kalo strict cuman bisa akseske link dari dan menuju domain yg sama, lax itu bisa dari domain lain tapi cuman bisa get
+          sameSite: "Strict", //ini ngatur domain yg request misal kalo strict cuman bisa akseske link dari dan menuju domain yg sama, lax itu bisa dari domain lain tapi cuman bisa get
           maxAge: 24 * 60 * 60 * 1000,
-          secure: false, //ini ngirim cookies cuman bisa dari https, kenapa? nyegah skema MITM di jaringan publik, tapi pas development di false in aja
+          secure: true, //ini ngirim cookies cuman bisa dari https, kenapa? nyegah skema MITM di jaringan publik, tapi pas development di false in aja
         });
         res.status(200).json({
           status: "Succes",
